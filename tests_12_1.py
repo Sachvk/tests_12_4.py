1789f7733bf62a93ac33e944ec21af6f25812ec2
Sachvk <serg22011985@gmail.com>
2024-12-05 15:51:34 +0300

    Update tests_12_1.py

--- tests_12_1.py
+++ tests_12_1.py
@@ -32,5 +32,8 @@ class RunnerTest(unittest.TestCase):
         self.assertNotEqual(test3.distance, test4.distance)
 
 
-logging.basicConfig(level='INFO', filemode='w', filename='runner_tests.log', encoding='UTF-8',
-                    format='%(asctime)s | %(levelname)s | %(message)s')
+if __name__ == 'main':
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8',
+                        format='%(asctime)s | %(levelname)s | %(message)s')
+    unittest.main()
+
